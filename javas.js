1b4038c3205049a49b11d28102b8401d53433060
pranava007 <103396008+pranava007@users.noreply.github.com>
2022-05-09 15:19:11 +0530

    javas.js
    js

---
+++ javas.js
@@ -0,0 +1,31 @@
+alert ("leavet muthu");
+//console
+console.log("HI");
+
+//for loop
+for (var i =10 ; i<30 ; i++)
+{
+	console.log(i);
+}
+
+
+//while loop
+
+var i1 = 10 ;
+while(i1<35)
+{
+	console.log(i1);
+	i1++;
+}
+*/
+//for Each
+
+var no3=[1,23,4,5];
+no3.forEach(function(no4)
+{
+
+console.log(no4);
+
+}
+
+); 
